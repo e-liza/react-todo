5df6d2b7995ade3eda1d6a648805aab16f1affaa
e-liza <eliza.lks@gmail.com>
2019-02-11 13:40:00 +0300

    Refactoring to class components, adding missing attributes and classes, adding state and functionality (done, important)

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './search.css';
 
 const Search = () => {
-  return <input placeholder="search" />;
+  return (
+    <input type="text" className="search-input" placeholder="Type to search" />
+  );
 };
 
 export default Search;
